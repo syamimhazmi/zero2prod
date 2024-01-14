5d48cc2a9c70f257d3b0ecea605b6feff2fac27c
syamimhazmi <syamimhazmi@gmail.com>
2024-01-15 04:55:30 +0700

    add health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new( || {
+        App::new()
+            .route("/healt-check", web::get().to(health_check))
+    })
+        .bind("127.0.0.1:9001")?
+        .run()
+        .await
 }
