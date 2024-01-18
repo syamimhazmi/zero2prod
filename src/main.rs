a3d03d05a7e979a735f4d0510d4bd7474ec4e8d5
syamimhazmi <syamimhazmi@gmail.com>
2024-01-19 00:53:14 +0700

    refactor logger into telemtery mod

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,14 @@
 use zero2prod::configuration::get_configuration;
 use zero2prod::startups::run;
+use zero2prod::telemetry::{init_subscriber, get_subscriber};
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-use tracing_log::LogTracer;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("Failed to set logger");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
 
-    let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info"));
-
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero2prod".into(),
-        std::io::stdout
-    );
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to load configuration file");
     let connection_pool = PgPool::connect(
