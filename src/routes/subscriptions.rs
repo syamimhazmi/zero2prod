b6325549a8d14ceaf22e3aa688bdd99ee83b91a5
syamimhazmi <syamimhazmi@gmail.com>
2024-01-26 01:54:32 +0700

    added AsRef traits

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,8 +2,7 @@ use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
 use chrono::Utc;
 use uuid::Uuid;
-use unicode_segmentation::UnicodeSegmentation;
-use crate::domain::NewSubscriber;
+use crate::domain::{NewSubscriber, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -23,7 +22,12 @@ pub async fn subscribes(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>
 ) -> HttpResponse {
-    match insert_subscriber(&pool, &form).await {
+    let new_subscriber = NewSubscriber {
+        email: form.0.email,
+        name: SubscriberName::parse(form.0.name)
+    };
+
+    match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish()
     }
@@ -31,13 +35,13 @@ pub async fn subscribes(
 
 #[tracing::instrument(
     name = "Saving new subscriber details in database",
-    skip(form, pool)
+    skip(new_subscriber, pool)
 )]
 pub async fn insert_subscriber(pool: &PgPool, new_subscriber: &NewSubscriber) -> Result<(), sqlx::Error> {
     sqlx::query!(r#"
         insert into subscriptions (id, email, name, subscribed_at)
         values ($1, $2, $3, $4)
-    "#, Uuid::new_v4(), new_subscriber.email, new_subscriber.name, Utc::now())
+    "#, Uuid::new_v4(), new_subscriber.email, new_subscriber.name.as_ref(), Utc::now())
         .execute(pool)
         .await
         .map(|e| {
