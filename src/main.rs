c9a42828a7edbcfb38a1229f9a4f8795e823e919
syamimhazmi <syamimhazmi@gmail.com>
2024-02-07 14:19:05 +0700

    refactor main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use zero2prod::configuration::get_configuration;
-use zero2prod::startups::run;
+use zero2prod::startups::build;
 use zero2prod::telemetry::{init_subscriber, get_subscriber};
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
-use zero2prod::email_client::EmailClient;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -12,24 +9,10 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to load configs file");
-    let connection_pool = PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.with_db());
 
-    let sender_email = configuration.email_client.sender().expect("Invalid sender email address");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout
-    );
+   let server = build(configuration).await?;
 
-    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
-
-    let listener = TcpListener::bind(address)?;
-
-    run(listener, connection_pool, email_client)?.await?;
+    server.await?;
 
     Ok(())
 }
