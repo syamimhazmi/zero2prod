9089f42d1bf95ea65333fbddda067c31049a33a6
syamimhazmi <syamimhazmi@gmail.com>
2024-02-14 10:23:17 +0700

    add newsletters endpoint

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -3,7 +3,7 @@ use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
 
 #[tokio::test]
-async fn newsletter_are_not_delivered_to_unconfirmed_subscriber() {
+async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
     let app = spawn_app().await;
 
     create_unconfirmed_subscriber(&app).await;
